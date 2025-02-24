5a9dc549533a73c35f4f6e7c10400a8d8192479e
henryedbris <ym24345@bris.ac.uk>
2025-02-24 11:28:48 +0000

    advance lecture

--- model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,8 +4,11 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.Optional;
 
 /**
  * cw-model
@@ -13,6 +16,60 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
+	private final class MyGameState implements GameState {
+		@Override public GameSetup getSetup() {  return null; }
+		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
+
+		@Nonnull
+		@Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getWinner() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
+		@Override public GameState advance(Move move) {
+
+			return move.accept(new Move.Visitor<GameState>() {
+				@Override
+				public GameState visit(Move.SingleMove move) {
+					// move.commencedBy();
+					// if mr x do mr x things
+					// if detective remove from remaining give ticket to mr x
+					return null;
+				}
+
+				@Override
+				public GameState visit(Move.DoubleMove move) {
+					// travel log - update twice
+					// remaining detectives
+					return null;
+				}
+			});
+		}
+	}
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
